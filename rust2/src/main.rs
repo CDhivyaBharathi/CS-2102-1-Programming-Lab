cdeb20fa13a9b22acd4daf47631667bd9ce3762a
CDhivyaBharathi <bharathi.chellakumar@gmail.com>
2025-11-09 23:55:10 +0530

    learn: rust version of malloc

---
+++ rust2/src/main.rs
@@ -0,0 +1,10 @@
+fn main(){
+    let mut v: Vec<i32> = Vec::with_capacity(10);
+    let mut j: Vec<i32> = Vec::new();
+    j.push(1);
+
+
+    //freeing is not needed as it is freed automatically after its out of scope
+
+
+}
